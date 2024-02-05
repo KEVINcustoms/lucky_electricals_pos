a25ec986e0034bdcd0a406386645d7b96700d528
NEXATEK <nexatekgroup@gmail.com>
2024-02-05 19:54:03 +0300

    I've also tried to add some code

--- testApp/src/testapp/TestApp.java
+++ testApp/src/testapp/TestApp.java
@@ -6,11 +6,18 @@ import javax.swing.JOptionPane;
 public class TestApp {
 
     public static void main(String[] args) {
+        TestApp test = new TestApp();
+        test.Subtraction(89, 14);
     int num1 = 67;
     int num2 =45;
     int sum = num1+num2;
     System.out.println("The sum of two numbers is "+sum);
     JOptionPane.showMessageDialog(null, "The sum of two numbers is "+sum);
+    }
+    public void Subtraction(int num1,int num2){
+    int difference;
+    difference = num1-num2;
+    JOptionPane.showMessageDialog(null, "The answer after subtraction of two numbers is "+difference);
     }
     /** Yooo this is KEVINcustoms online advertising company and we thank you for reaching out please record you voice mail message after the beat**/
     
