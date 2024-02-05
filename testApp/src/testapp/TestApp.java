85f3dd8002fd1c8d23f3e2c5b9aff4a5356b020a
NEXATEK <nexatekgroup@gmail.com>
2024-02-05 17:46:46 +0300

    this is what i dont remember

--- testApp/src/testapp/TestApp.java
+++ testApp/src/testapp/TestApp.java
@@ -1,6 +1,8 @@
 
 package testapp;
 
+import javax.swing.JOptionPane;
+
 public class TestApp {
 
     public static void main(String[] args) {
@@ -8,6 +10,7 @@ public class TestApp {
     int num2 =45;
     int sum = num1+num2;
     System.out.println("The sum of two numbers is "+sum);
+    JOptionPane.showMessageDialog(null, "The sum of two numbers is "+sum);
     }
     /** Yooo this is KEVINcustoms online advertising company and we thank you for reaching out please record you voice mail message after the beat**/
     
